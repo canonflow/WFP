8460701006dc59a2442c20c4f3aae684ff3b4ff7
canonflow <nathangarzyasantoso7@gmail.com>
2025-02-24 12:03:12 +0700

    END WEEK 2

--- week2/routes/web.php
+++ week2/routes/web.php
@@ -16,3 +16,95 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::view("/custom", "welcome");
+
+// Route::get("/user/{id?}", function($id = "Default") {
+//     return "User ID: $id";
+// });
+
+// ===== CARA 1 =====
+// Route::get("/", function() {
+//     return view("welcome");
+// });
+
+// Route::get("/{toko}", function($toko) {
+//     return "Halaman <span style='color: red;'>$toko</span>";
+// });
+
+// Route::get('/{toko}/{product}', function($toko, $product) {
+//     return "Halaman <span style='color: red;'>$toko</span> dengan produk <span style='color: blue;'>$product</span>";
+// });
+
+// ===== CARA 2 =====
+// Route::get("/search", function() {
+//     return "Halaman search";
+// });
+
+// Route::get("/register", function() {
+//     return "Halaman register";
+// });
+
+// Route::get("/{toko?}/{product?}", function($toko = null, $product = null) {
+//     if (!$toko) {
+//         return view("welcome");
+//     } else if (!$product) {
+//         return "Halaman <span style='color: red;'>$toko</span>"; 
+//     }
+
+//     return "Halaman <span style='color: red;'>$toko</span> dengan produk <span style='color: blue;'>$product</span>";
+// });
+
+// ===== PRAKTIK 1 =====
+// Route::get('/welcome', function() {
+//     return "<h1>Selamat Datang</h1>";
+// })->name('welcome');
+
+// Route::get("before_order", function() {
+//     return "<h1>Pilih DINE-IN atau TAKE AWAY</h1>";
+// })->name("before-order");
+
+// Route::get('menu/{type}', function($type) {
+//     // $type = Str::title($type);
+//     $type = (strtolower($type) == 'dinein') ? 'Dine-in' : 'Take-away';
+//     return "<h1>Daftar menu $type</h1>";
+// })->name('menu');
+
+// Route::get('/admin/{page}', function($page) {
+//     $page = ucfirst($page);
+//     return "<h1>Portal Manajemen: Daftar $page</h1>";
+// })->name('admin.page');
+
+// ===== VIEW =====
+Route::get('/home', function() {
+    return view("home", [
+        "name" => "Nathan Ganteng",
+        "angka" => 1
+    ]);
+});
+
+// ===== PRAKTIK 2 =====
+Route::get("/welcome", function() {
+    $link = route("order");
+    return "<h1>Splash screen: HealthyFood<br /><br /><a href='$link'>Start Order</a></h1>";
+});
+Route::get("/before_order", function() {
+    $linkDinein = route("menu", ['type' => 'dinein']);
+    $linkTakeaway = route("menu", ['type' => 'takeaway']);
+    return "<a href='$linkDinein'>Dine In</a><br /><a href='$linkTakeaway'>Take Away</a>";
+})->name("order");
+
+Route::get("/menu/{type}", function($type) {
+    $type = strtolower($type);
+
+    if (in_array($type, ['dinein', 'takeaway'])) {
+        return view("menu", ['type' => $type]);
+    }
+    
+    abort(404);
+})->name("menu");
+
+
+
+
+
